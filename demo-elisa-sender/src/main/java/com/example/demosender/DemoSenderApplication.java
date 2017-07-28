29a19d4ff828f0127895614f148afcaee5480a66
Martin Bonev <martin.bonev@rewe-digital.com>
2017-07-28 15:22:32 +0300

    Sending the text directly from the REST to the jms queue

--- demo-elisa-sender/src/main/java/com/example/demosender/DemoSenderApplication.java
+++ demo-elisa-sender/src/main/java/com/example/demosender/DemoSenderApplication.java
@@ -2,28 +2,21 @@ package com.example.demosender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jms.support.converter.MarshallingMessageConverter;
-import org.springframework.jms.support.converter.MessageConverter;
-import org.springframework.jms.support.converter.MessageType;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class DemoSenderApplication {
 
 	public static void main(String[] args) {
         SpringApplication.run(DemoSenderApplication.class, args);
     }
 
-    @Bean
-    public MessageConverter getMessageConverter() {
-        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-        Class<?>[] classesToBeBound = {Activity.class};
-        marshaller.setClassesToBeBound(classesToBeBound);
-        MarshallingMessageConverter converter = new MarshallingMessageConverter(marshaller);
-        converter.setTargetType(MessageType.TEXT);
-        return converter;
-    }
+//    @Bean
+//    public MessageConverter getMessageConverter() {
+//        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+//        Class<?>[] classesToBeBound = {PredictionMessage.class};
+//        marshaller.setClassesToBeBound(classesToBeBound);
+//        MarshallingMessageConverter converter = new MarshallingMessageConverter(marshaller);
+//        converter.setTargetType(MessageType.TEXT);
+//        return converter;
+//    }
 }
